25b4d9fe1949a9b781cbf3f1ca0c7598ac2641e0
Matt Wood <mwood@mulling.net>
2014-10-19 20:25:44 -0700

    get rid of users and unused query

--- routes/trades.js
+++ routes/trades.js
@@ -3,12 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    req.app.locals.connection(function(err, conn) {
-        conn.query("select * from owners", function(err, rows){
-            console.log(err, rows);
-            conn.release(); 
-        });
-    });
   res.render('trades', { title: 'Trade Board' });
 });
 
